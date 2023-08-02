3e7ae92196e9a68f163ae857c4977f3f992af946
Leonardo Pedrosa <leonardo.pedrosa@take.net>
2023-08-01 21:50:30 -0300

    Add axios and useEffect to get pokemon list from api

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Pokemon = () => {
+  const [pokemons, setPokemons] = useState([]);  
+  
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon/')
+    .then(res => {
+      setPokemons(res.data.results);
+      console.log(res.data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }, []);
+
   return (
-    <div>
-      <h2>Pokemon</h2>
-      <p>Welcome to the Pokemon page!</p>
-    </div>
+      <div>
+        <h2>Pokemon List</h2>
+        <p>Welcome to the Pokemon page!</p>
+          <ul>
+            {pokemons.map(pokemon => (
+              <li key={pokemon.name}>{pokemon.name}</li>
+          ))}
+        </ul>
+      </div>
   );
 };
 
