217491e21bf03d5957f769e371d91750b4850ecc
Leonardo Pedrosa <leonardo.pedrosa@take.net>
2023-08-01 21:39:17 -0300

    first commit

---
+++ src/components/Pokemon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Pokemon = () => {
+  return (
+    <div>
+      <h2>Pokemon</h2>
+      <p>Welcome to the Pokemon page!</p>
+    </div>
+  );
+};
+
+export default Pokemon;
